82d8cc3a6b83681e47995fb598bf3dfc9df7caad
User1 <user1@Margirovski-MacBook-Pro.local>
2026-05-30 23:16:46 +0200

    fix duplicate firebase exports

--- src/firebase.js
+++ src/firebase.js
@@ -1,41 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
 import { getMessaging } from "firebase/messaging";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBDJ05hxbI4hkiAOGu9FMJuWykDbx5LtF0",
-  authDomain: "dobro-spienje.firebaseapp.com",
-  projectId: "dobro-spienje",
-  storageBucket: "dobro-spienje.firebasestorage.app",
-  messagingSenderId: "553680058242",
-  appId: "1:553680058242:web:49b72eaa9cb35e7e74019b",
-  databaseURL: "https://dobro-spienje-default-rtdb.europe-west1.firebasedatabase.app/"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
