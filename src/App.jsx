50fcf49ad0b37ca829431f7b76f77f15ceea0ebc
User1 <user1@Margirovski-MacBook-Pro.local>
2026-05-31 16:57:56 +0200

    notifications

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ console.log("all members:", members);
 
     if (otherTokens.length === 0) return;
 
-const response = await fetch("/api/notify.cjs", {
+const response = await fetch("/api/notify", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ tokens: otherTokens, name, isAsleep }),
